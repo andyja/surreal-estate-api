41caee5638227420b3128c13ba68d46bece8b8d1
Andy Jackson <andypauljackson@gmail.com>
2023-05-19 12:37:50 +0100

    express

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const app = require('express')();
+const https = require('https');
+const http = require('http');
+
+app.set('port', 443);
 
 app.get('/api', (req, res) => {
   const path = `/api/item/test`;
@@ -6,3 +10,8 @@ app.get('/api', (req, res) => {
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: `);
 });
+
+var server = https.createServer(app)
+server.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'))
+})
