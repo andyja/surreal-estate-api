9b1f5143ccc1cb7955d023d8a8513b63b7a47383
Andy Jackson <andypauljackson@gmail.com>
2023-05-23 12:36:01 +0100

    change of

--- api/index.js
+++ api/index.js
@@ -1,17 +1,3 @@
-const app = require('express')();
-const https = require('https');
-const http = require('http');
+const { app } = require("../index");
 
-app.set('port', 443);
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/test`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: `);
-});
-
-// var server = https.createServer(app)
-// server.listen(app.get('port'), () => {
-//   console.log('Express server listening on port ' + app.get('port'))
-// })
+module.exports = app;
--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-const app = require('express')();
-const https = require('https');
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const restify = require('express-restify-mongoose');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const expressListRoutes   = require('express-list-routes');
+const cors = require('cors');
+const PropertyListingModel = require('../models/property');
+const FavouriteModel = require('../models/favourite');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
-app.set('port', 443);
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/test`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: `);
+require('dotenv').config({
+  path: path.join(__dirname, './.env'),
 });
 
-// var server = https.createServer(app)
-// server.listen(app.get('port'), () => {
-//   console.log('Express server listening on port ' + app.get('port'))
-// })
+app.use(cors({ credentials: true, origin: true }));
+app.options('*', cors());
+app.use(express.json());
+app.use(methodOverride());
+
+
+mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true });
+
+restify.serve(router, PropertyListingModel);
+restify.serve(router, FavouriteModel);
+
+app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
+
+app.get('/api/v1/test', (req, res) => {
+  res.end(`Item: hello world}`);
+});
+
+expressListRoutes({}, 'Endpoints:', router );
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Surreal Estate API is running on :${PORT}`);
+});
+
+module.exports.app = app;
